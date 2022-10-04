b96dfb33efe5fd88feefac1e21831fb1905b8bc5
Ajay Pandey <57106804+panditajay73@users.noreply.github.com>
2022-10-04 19:02:47 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Armstrong {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//int n =sc.nextInt();
+		//System.out.println(isArmstrong(n));
+		
+		//check no of armstrongs from 0 to 1000
+
+		for(int i=0;i<1000;i++) {
+			if(isArmstrong(i)) {
+				System.out.print(i+" ");
+			}
+		}
+		sc.close();
+	}
+	static boolean isArmstrong(int n) {
+		int original=n;
+		int sum=0;
+		while(n>0) {
+			int rem = n%10;
+			n=n/10;
+			sum = sum+rem*rem*rem;	
+		}
+		return sum==original;
+	}
+
+}
